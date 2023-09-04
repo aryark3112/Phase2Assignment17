3b8320faa42056c358db07b27662a077d1f980bc
aryark <aryark3112@gmail.com>
2023-09-04 14:46:17 +0530

    first commit

---
+++ Assignment17/Assignment17/Data/MovieseDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment17.Models;
+
+namespace Assignment17.Data
+{
+    public class MovieseDbContext : DbContext
+    {
+        public MovieseDbContext (DbContextOptions<MovieseDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment17.Models.Movie> Movie { get; set; } = default!;
+    }
+}
